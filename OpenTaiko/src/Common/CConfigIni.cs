b7d7145c813711ae4419855012293da9e9553e16
Takkkom <takukom@outlook.jp>
2023-09-08 11:25:02 +0900

    update

--- OpenTaiko/src/Common/CConfigIni.cs
+++ OpenTaiko/src/Common/CConfigIni.cs
@@ -1896,7 +1896,7 @@ namespace TJAPlayer3
 			this.bTight = false;                        // #29500 2012.9.11 kairera0467 TIGHTモード
 			#region [ WASAPI/ASIO ]
 			this.nSoundDeviceType = (int)ESoundDeviceTypeForConfig.Bass;	// #24820 2012.12.23 yyagi 初期値はACM | #31927 2013.8.25 yyagi OSにより初期値変更
-			nBassBufferSizeMs = 10;
+			nBassBufferSizeMs = 1;
 			this.nWASAPIBufferSizeMs = 50;				// #24820 2013.1.15 yyagi 初期値は50(0で自動設定)
 			this.nASIODevice = 0;						// #24820 2013.1.17 yyagi
 //			this.nASIOBufferSizeMs = 0;					// #24820 2012.12.25 yyagi 初期値は0(自動設定)
